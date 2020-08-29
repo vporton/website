df1bb3a436163f3ce83b9c66c484c6c855dde694
Cedrik <cedrik.git@tryninja.io>
2020-08-29 13:18:08 -0500

    🐛added Sentry

--- src/assets/scripts/global.ts
+++ src/assets/scripts/global.ts
@@ -2,6 +2,17 @@
  * This one is used on all parts of the admin panel.
  */
 
+import * as Sentry from '@sentry/browser';
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: 'https://797cc582ba794c93934d01bf44722e96@o440904.ingest.sentry.io/5410617',
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 import * as feather from "feather-icons";
 import $ from './libs/jquery';
 
