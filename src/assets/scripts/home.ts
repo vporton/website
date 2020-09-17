21d8376e56173966baf19db2e9c593aff523e442
Cedrik <cedrik.git@tryninja.io>
2020-09-17 15:06:24 -0500

    ✨display communities

--- src/assets/scripts/home.ts
+++ src/assets/scripts/home.ts
@@ -7,4 +7,5 @@ import './global';
 $(() => {
   $('a.create').attr('href', './create.html');
   $('a.opp').attr('href', './opportunity.html');
+  $('a.comms').attr('href', './communities.html');
 });
